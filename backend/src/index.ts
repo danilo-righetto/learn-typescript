2bd19bd97d837f22b6e01cfe013231b5732dc00e
Danilo Righetto <danilonewtrue@gmail.com>
2020-05-28 13:58:03 -0300

    Criando o projeto frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
+app.use(cors());
 app.use(routes);
+
 
 /* As bibliotecas precisam expor as suas tipagens */
 app.listen(3333);
